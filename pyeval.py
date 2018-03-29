c78d99ca6dad7ffeeff8d8b39ca7d61a3560302a
Rado Kirov <rkirov@gmail.com>
2018-03-29 15:46:22 -0700

    Upgrade to webapp2
    Run autopep8 on pyeval.py, run clang-format on index.js.

--- pyeval.py
+++ pyeval.py
@@ -8,19 +8,18 @@ Running as back process using App Engine queues
 import os
 import sys
 import traceback
-import wsgiref.handlers
 import time
 import StringIO
-import json 
+import json
 import logging
 
+import webapp2
 from google.appengine.ext import db
-from google.appengine.ext import webapp
-from google.appengine.api.datastore import Key 
+from google.appengine.api.datastore import Key
 from google.appengine.api import taskqueue
 
-
 namespace = {}
+
 
 def unicode_str(obj, encoding='utf-8'):
     """Takes an object and returns a unicode human-readable representation."""
@@ -30,22 +29,24 @@ def unicode_str(obj, encoding='utf-8'):
         return obj
     return unicode(obj)
 
+
 class ComputeCell(db.Model):
     exec_string = db.TextProperty()
     output = db.TextProperty()
     status = db.StringProperty()
 
-class ExecuteWorker(webapp.RequestHandler):
+
+class ExecuteWorker(webapp2.RequestHandler):
     # TODO: add some sort of bulking mechanism
     def post(self):
         key = self.request.get('key')
-        cell = db.get(key) 
+        cell = db.get(key)
         out = self.execute_code(cell.exec_string)
         cell.status = 'done'
         cell.output = out
         cell.put()
 
-    def execute_code(self,code):
+    def execute_code(self, code):
         """Evalue the given code, returning what is sent to stdout."""
         ans = ''
         logging.debug('running', code)
@@ -56,7 +57,7 @@ class ExecuteWorker(webapp.RequestHandler):
         try:
             exec code in namespace
         except:
-            ans += traceback.format_exc() 
+            ans += traceback.format_exc()
         finally:
             sys.stdout.seek(0)
             sys.stderr.seek(0)
@@ -66,35 +67,40 @@ class ExecuteWorker(webapp.RequestHandler):
         logging.debug('output', ans)
         return ans
 
-class AJAXHandler(webapp.RequestHandler):
+
+class AJAXHandler(webapp2.RequestHandler):
     def get(self):
         key = Key(self.request.get('id'))
-        cell = db.get(key) 
-        self.response.out.write(json.dumps({'status':cell.status,
-                                          'output':cell.output
-                                         }))
+        cell = db.get(key)
+        self.response.out.write(json.dumps({'status': cell.status,
+                                            'output': cell.output
+                                            }))
 
     def post(self):
         self.response.headers['Content-Type'] = 'text/plain'
         new_cell = ComputeCell(exec_string=self.request.get('input'),
-                    status='not ready', output='')
+                               status='not ready', output='')
         new_cell.put()
         key = new_cell.key()
-        taskqueue.add(url='/worker', params={'key':key})
+        taskqueue.add(url='/worker', params={'key': key})
         self.response.out.write(key)
 
-class GetID(webapp.RequestHandler):
+
+class GetID(webapp2.RequestHandler):
     def get(self):
         key = Key(self.request.get('id'))
-        cell = db.get(key) 
-        self.response.out.write(json.dumps({'exec_string':cell.exec_string}))
+        cell = db.get(key)
+        self.response.out.write(json.dumps({'exec_string': cell.exec_string}))
+
 
 def main():
-    application = webapp.WSGIApplication(
-        [ ('/exec', AJAXHandler),
-          ('/get_id', GetID),
-          ('/worker', ExecuteWorker)], debug=True)
-    wsgiref.handlers.CGIHandler().run(application)
+    application = webapp2.WSGIApplication([
+        webapp2.Route('/', webapp2.RedirectHandler,
+                      defaults={'_uri': 'static/index.html'}),
+        ('/exec', AJAXHandler),
+        ('/get_id', GetID),
+        ('/worker', ExecuteWorker)], debug=True)
+    application.run()
 
 
 if __name__ == '__main__':
--- static/index.js
+++ static/index.js
@@ -1,105 +1,94 @@
-function getParameterByName(name)
-{
-  name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
-  var regexS = "[\\?&]" + name + "=([^&#]*)";
-  var regex = new RegExp(regexS);
-  var results = regex.exec(window.location.href);
-  if(results == null)
-    return "";
-  else
-    return decodeURIComponent(results[1].replace(/\+/g, " "));
-}
+
+(function() {
+var poller, editor;
+
 /**
  * A simple querystring parser.
- * Example usage: var q = $.parseQuery(); q.fooreturns  "bar" if query contains "?foo=bar"; multiple values are added to an array. 
-/* ORIGNAL CODE
- * Radoslav Kirov
- * */
-
-(function (){
-var poller, editor; 
-
-function Poller(idin, waittimein){
-    this.id = idin;
-    this.waittime = waittimein || 1000;
-    this.setTimeout = setTimeout(poll, waittimein);
+ * Example usage: var q = $.parseQuery(); q.fooreturns  'bar' if query contains
+'?foo=bar'; multiple values are added to an array.
+ */
+function getParameterByName(name) {
+  name = name.replace(/[\[]/, '\\\[').replace(/[\]]/, '\\\]');
+  var regexS = '[\\?&]' + name + '=([^&#]*)';
+  var regex = new RegExp(regexS);
+  var results = regex.exec(window.location.href);
+  if (results == null) {
+    return '';
+  } else {
+    return decodeURIComponent(results[1].replace(/\+/g, ' '));
+  }
 }
 
-poll = function (){
-    $.ajax({
-        url: '/exec',
-        data: {
-            id: poller.id 
-        },
-        success: function (raw_data){
-            data = JSON.parse(raw_data);
-            if (data.status !== 'not ready'){
-                $('#permalink').html('<a href="index.html?id=' + poller.id + '">permlink</a>');
-                $('#loader').hide(); 
-                $('#answer').show(); 
-                $('#answer').html(data.output); 
-            } else {
-                poller.settimeout = setTimeout(poll, poller.waittime);
-            }
-        }                     
-    });
-
+function Poller(idin, waittimein) {
+  this.id = idin;
+  this.waittime = waittimein || 1000;
+  this.setTimeout = setTimeout(poll, waittimein);
 }
 
-function execute(input){
-    var input = input || editor.getValue(); 
-    // var input = input || $('#input').val(); 
-    //console.log('exec', input);
-    $.ajax({
-        url: '/exec',
-        type: 'POST',
-        data: {
-            input: input
-        },
-        success: function (data){
-            /* console.log('entered with id', data); */
-            poller = new Poller(data);
-            $('#loader').show();
-            $('#input').attr("disabled","disabled");
-        }                     
-    });
-}
-
-$(document).ready(function (){
-    editor = CodeMirror.fromTextArea(document.getElementById("input"), {
-        mode: {name: "python",
-        version: 2,
-        singleLineStringErrors: false},
-        lineNumbers: true,
-        indentUnit: 4,
-        tabMode: "shift",
-        matchBrackets: true,
-        onKeyEvent: 
-        function (editor, e){
-            if (e.keyCode === 13 && e.shiftKey){
-                execute();
-                e.stop();
-            }
-        }
-    });
-    $('#loader').hide();
-    $('#compute_button').click(function (){
-        execute();
-    });
-    var id = getParameterByName('id'); 
-    if (id){
-        $.ajax({
-            url: '/get_id',
-            type: 'GET',
-            data: {
-                id: id 
-            },
-            success: function (raw_data){
-                data = JSON.parse(raw_data);
-                editor.setValue(data.exec_string);
-            }                     
-        });
-        poller = new Poller(id);
+function poll() {
+  $.ajax({
+    url : '/exec',
+    data : {id : poller.id},
+    success : function(raw_data) {
+      data = JSON.parse(raw_data);
+      if (data.status !== 'not ready') {
+        $('#permalink')
+            .html('<a href="index.html?id=' + poller.id + '">permlink</a>');
+        $('#loader').hide();
+        $('#answer').show();
+        $('#answer').html(data.output);
+      } else {
+        poller.settimeout = setTimeout(poll, poller.waittime);
+      }
     }
+  });
+}
+
+function execute(input) {
+  var input = input || editor.getValue();
+  // var input = input || $('#input').val();
+  // console.log('exec', input);
+  $.ajax({
+    url : '/exec',
+    type : 'POST',
+    data : {input : input},
+    success : function(data) {
+      /* console.log('entered with id', data); */
+      poller = new Poller(data);
+      $('#loader').show();
+      $('#input').attr('disabled', 'disabled');
+    }
+  });
+}
+
+$(document).ready(function() {
+  editor = CodeMirror.fromTextArea(document.getElementById('input'), {
+    mode : {name : 'python', version : 2, singleLineStringErrors : false},
+    lineNumbers : true,
+    indentUnit : 4,
+    tabMode : 'shift',
+    matchBrackets : true,
+    onKeyEvent : function(editor, e) {
+      if (e.keyCode === 13 && e.shiftKey) {
+        execute();
+        e.stop();
+      }
+    }
+  });
+  $('#loader').hide();
+  $('#compute_button').click(function() { execute(); });
+  var id = getParameterByName('id');
+  if (id) {
+    $.ajax({
+      url : '/get_id',
+      type : 'GET',
+      data : {id : id},
+      success : function(raw_data) {
+        data = JSON.parse(raw_data);
+        editor.setValue(data.exec_string);
+      }
+    });
+    poller = new Poller(id);
+  }
 });
 })();
